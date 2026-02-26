c6860e661afee4ef6759008bf72d511c750ba8fb
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2026-02-26 14:09:14 +0100

    update description

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -1,33 +1,27 @@
 const projectDetails = [
-    {
-        title:"TICKET MANAGEMENT APP",
-        
-        description:"Build with React",
-        demoUrl: 'https://tickets-app-react-v.netlify.app/',
-    },
-    {
-         title:"CHUCKS KITCHEN APP",
-        description:"Build with React",
-        demoUrl: 'https://chucks-kitchen-alpha.vercel.app/',
-    }, 
-   {
-        title:"GETCHANGE CLONE",
-             description:'I designed this project using vue.js and tailwindcss',
-        demoUrl: "https://getchangeclone-ke2o0gdjp-muritala-sheriffdeen-adegokes-projects.vercel.app/"
-    },
-    {
-        title:"LEZADA CLONE",
-       
-        description:'LEZADA is an e-commerce website that I replicate',
-        demoUrl: 'https://lezadaclone.vercel.app/'
-    },
-   
-    
-   
-   
-]
+  {
+    title: "TICKET MANAGEMENT APP",
 
+    description: "Build with React",
+    demoUrl: "https://tickets-app-react-v.netlify.app/",
+  },
+  {
+    title: "CHUCKS KITCHEN APP",
+    description: "Chuks Kitchen is a modern, full-stack web application designed to bring the authentic taste of Nigerian home cooking to the digital space. Built with a focus on performance, responsiveness, and a premium user experience, the platform serves as a bridge between traditional culinary heritage and contemporary convenience.",
+    demoUrl: "https://chucks-kitchen-alpha.vercel.app/",
+  },
+  {
+    title: "GETCHANGE CLONE",
+    description: "I designed this project using vue.js and tailwindcss",
+    demoUrl:
+      "https://getchangeclone-ke2o0gdjp-muritala-sheriffdeen-adegokes-projects.vercel.app/",
+  },
+  {
+    title: "LEZADA CLONE",
+
+    description: "LEZADA is an e-commerce website that I replicate",
+    demoUrl: "https://lezadaclone.vercel.app/",
+  },
+];
 
 export default projectDetails;
-
-
